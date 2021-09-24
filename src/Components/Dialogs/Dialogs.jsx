4f74d5b5ad4c8bff889261a7fd8a0a97f786e878
Albert <albert.barsamov@gmail.com>
2021-09-24 18:37:29 +0200

    DIalogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import style from "./Dialogs.module.css";
 export default function Dialogs(props) {
-  return <div>Dialogs</div>;
+  return (
+    <div className={style.dialogs}>
+      <div className={style.dialogs_items}>
+        <div className={style.dialog}>Jean</div>
+        <div className={`${style.dialog} ${style.active}`}>Paul</div>
+        <div className={style.dialog}>Claire</div>
+        <div className={style.dialog}>Sophie</div>
+        <div className={style.dialog}>Henry</div>
+      </div>
+
+      <div className={style.messages}>
+        <div className={style.message}>Hi</div>
+        <div className={style.message}>How are you?</div>
+        <div className={style.message}>YO!</div>
+      </div>
+    </div>
+  );
 }
