34e165ce6ca8678758da287bced6e0d14c72b44e
Albert <albert.barsamov@gmail.com>
2021-09-24 19:38:11 +0200

    navlink dialog

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
         <Header />
         <Nav />
         <div className="content">
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/dialogs" component={Dialogs} />
-          <Route exact path="/news" component={News} />
-          <Route exact path="/music" component={Music} />
-          <Route exact path="/settings" component={Settings} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
         </div>
       </div>
     </BrowserRouter>
