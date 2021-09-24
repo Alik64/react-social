1ab7a5a992bc0b557fdd9571939629e9801d92b3
Albert <albert.barsamov@gmail.com>
2021-09-24 19:38:02 +0200

    navlink dialog

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import style from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
 export default function Dialogs(props) {
   return (
     <div className={style.dialogs}>
       <div className={style.dialogs_items}>
-        <div className={style.dialog}>Jean</div>
-        <div className={`${style.dialog} ${style.active}`}>Paul</div>
-        <div className={style.dialog}>Claire</div>
-        <div className={style.dialog}>Sophie</div>
-        <div className={style.dialog}>Henry</div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/1" activeClassName={style.active}>
+            Jean
+          </NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/2" activeClassName={style.active}>
+            Paul
+          </NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/3" activeClassName={style.active}>
+            Claire
+          </NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/4" activeClassName={style.active}>
+            Sophie
+          </NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/5" activeClassName={style.active}>
+            Henry
+          </NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/6" activeClassName={style.active}>
+            Yvan
+          </NavLink>
+        </div>
       </div>
 
       <div className={style.messages}>
