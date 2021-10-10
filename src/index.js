520bea130e5f7578a635e2398d53da76e6bb6842
Albert <albert.barsamov@gmail.com>
2021-10-10 22:42:33 +0200

    Deplacer BrowserRouter vers index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import state from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App state={state} />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App state={state} />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
