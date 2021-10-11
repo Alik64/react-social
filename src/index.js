797009532481347b466756b281ea6d38dc63658b
Albert <albert.barsamov@gmail.com>
2021-10-11 15:51:55 +0200

    tst addpost

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import state from "./redux/state";
+import { addPost } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
+addPost("yo mf");
 ReactDOM.render(
   <BrowserRouter>
     <App state={state} />
