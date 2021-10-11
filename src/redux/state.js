6010878b391a7f2f07c4d44b178888b8c5bb461d
Albert <albert.barsamov@gmail.com>
2021-10-11 11:40:31 +0200

    +2 messages dans State.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -3,6 +3,8 @@ let state = {
     posts: [
       { id: 1, message: "Hi, how are you?", likesCount: "23" },
       { id: 2, message: "It's my first post", likesCount: "15" },
+      { id: 3, message: "Bla bla bla", likesCount: "22" },
+      { id: 4, message: "DA dA Da", likesCount: "11" },
     ],
   },
   dialogsPage: {
