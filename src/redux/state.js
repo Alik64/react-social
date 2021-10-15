a785820b4c0e4f5b84e7ecdac89652cb11c1e078
Albert <albert.barsamov@gmail.com>
2021-10-15 16:00:41 +0200

    regrouper les infos dans un seul objet Store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,76 +1,73 @@
-let rerenderEntireTree = () => {
-  console.log("rerender from state.js");
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: 23 },
-      { id: 2, message: "It's my first post", likesCount: 15 },
-      { id: 3, message: "Bla bla bla", likesCount: 22 },
-      { id: 4, message: "DA dA Da", likesCount: 11 },
-    ],
-    newPostText: "Redux copy",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 23 },
+        { id: 2, message: "It's my first post", likesCount: 15 },
+        { id: 3, message: "Bla bla bla", likesCount: 22 },
+        { id: 4, message: "DA dA Da", likesCount: 11 },
+      ],
+      newPostText: "Redux copy",
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Paul", sexe: "M" },
+        { id: 2, name: "Marie", sexe: "F" },
+        { id: 3, name: "Bruno", sexe: "M" },
+        { id: 4, name: "David", sexe: "M" },
+        { id: 5, name: "Sophie", sexe: "F" },
+        { id: 6, name: "Leo", sexe: "M" },
+      ],
+      messages: [
+        { id: 1, msg: "Hi how are you?" },
+        { id: 2, msg: "I'm fine, thnx!" },
+        { id: 3, msg: "YO" },
+        { id: 4, msg: "What's up" },
+        { id: 5, msg: "Yo!" },
+      ],
+      newMessageText: "Enter your message",
+    },
+    nav: {
+      friends: [
+        { id: 1, name: "Paul", sexe: "M" },
+        { id: 2, name: "Marie", sexe: "F" },
+        { id: 3, name: "Bruno", sexe: "M" },
+        { id: 4, name: "David", sexe: "M" },
+      ],
+    },
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Paul", sexe: "M" },
-      { id: 2, name: "Marie", sexe: "F" },
-      { id: 3, name: "Bruno", sexe: "M" },
-      { id: 4, name: "David", sexe: "M" },
-      { id: 5, name: "Sophie", sexe: "F" },
-      { id: 6, name: "Leo", sexe: "M" },
-    ],
-    messages: [
-      { id: 1, msg: "Hi how are you?" },
-      { id: 2, msg: "I'm fine, thnx!" },
-      { id: 3, msg: "YO" },
-      { id: 4, msg: "What's up" },
-      { id: 5, msg: "Yo!" },
-    ],
-    newMessageText: "Enter your message",
+  rerenderEntireTree() {
+    console.log("rerender from state.js");
   },
-  nav: {
-    friends: [
-      { id: 1, name: "Paul", sexe: "M" },
-      { id: 2, name: "Marie", sexe: "F" },
-      { id: 3, name: "Bruno", sexe: "M" },
-      { id: 4, name: "David", sexe: "M" },
-    ],
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    state.profilePage.posts.unshift(newPost);
+    state.profilePage.newPostText = "";
+    rerenderEntireTree(state);
+  },
+  updateNewPostChange(newText) {
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
+  },
+  addMessage() {
+    let newMessage = { id: 6, msg: state.dialogsPage.newMessageText };
+    state.dialogsPage.messages.push(newMessage);
+    state.dialogsPage.newMessageText = "";
+    rerenderEntireTree(state);
+  },
+  updateNewMessageText(newText) {
+    state.dialogsPage.newMessageText = newText;
+    rerenderEntireTree(state);
+  },
+  subscribe(observer) {
+    rerenderEntireTree = observer;
   },
 };
 
-window.state = state;
+window.store = store;
 
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.unshift(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostChange = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const addMessage = () => {
-  let newMessage = { id: 6, msg: state.dialogsPage.newMessageText };
-  state.dialogsPage.messages.push(newMessage);
-  state.dialogsPage.newMessageText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newText) => {
-  state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
 export default state;
