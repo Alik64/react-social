fff5f857e35fe16bb3e192a3fc4bcd38e6dca65a
Albert <albert.barsamov@gmail.com>
2021-10-16 16:34:05 +0200

    simplifier action creator

--- src/redux/state.js
+++ src/redux/state.js
@@ -80,16 +80,14 @@ let store = {
     }
   },
 };
+// ACTION CREATORS
 
-export const addPostActionCreator = () => {
-  return { type: ADD_POST };
-};
-export const updateNewPostActionCreator = (text) => {
-  return {
-    type: UPDATE_NEW_POST,
-    newText: text,
-  };
-};
+export const addPostActionCreator = () => ({ type: ADD_POST });
+
+export const updateNewPostActionCreator = (text) => ({
+  type: UPDATE_NEW_POST,
+  newText: text,
+});
 
 window.store = store;
 
