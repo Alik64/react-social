8b17dd9cfd7973dc2ac567241616ac762813d8e9
Albert <albert.barsamov@gmail.com>
2021-10-17 11:30:29 +0200

    creation de contenu des Reduceurs

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,3 +1,14 @@
+const NEW_MESSAGE = "NEW-MESSAGE";
+const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
+
 const dialogsReducer = (state, action) => {
+  if (action.type === NEW_MESSAGE) {
+    let body = state.newMessageText;
+    state.newMessageText = "";
+    state.messages.push({ id: 6, msg: body });
+  } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+    state.newMessageText = action.newText;
+  }
+
   return state;
 };
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,3 +1,17 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST = "UPDATE-NEW-POST";
+
 const profileReducer = (state, action) => {
+  if (action.type === ADD_POST) {
+    let newPost = {
+      id: 5,
+      message: state.newPostText, // this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    state.posts.unshift(newPost);
+    state.newPostText = "";
+  } else if (action.type === UPDATE_NEW_POST) {
+    state.newPostText = action.newText;
+  }
   return state;
 };
