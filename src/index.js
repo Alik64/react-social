daa1f2985c5e5290c7388b799d1eecb41756fa80
Albert <albert.barsamov@gmail.com>
2021-10-26 17:59:20 +0200

    refactoring - index.js

--- src/index.js
+++ src/index.js
@@ -6,20 +6,12 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-let rerenderEntireTree = () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 
-    document.getElementById("root")
-  );
-};
-
-rerenderEntireTree();
-
-store.subscribe(() => {
-  rerenderEntireTree();
-});
+  document.getElementById("root")
+);
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -11,4 +11,6 @@ let reducers = combineReducers({
 
 const store = createStore(reducers);
 
+window.store = store;
+
 export default store;
