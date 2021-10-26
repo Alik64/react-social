272e49d07522439ac7fa25b6bfc266f3aafe2e34
Albert <albert.barsamov@gmail.com>
2021-10-26 14:25:31 +0200

    delete - StoreContext

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "./StoreContext";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 let rerenderEntireTree = () => {
