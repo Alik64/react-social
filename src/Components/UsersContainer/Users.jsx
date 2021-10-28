b63daf23157d7df97493230a4233dc266b38a696
Albert <albert.barsamov@gmail.com>
2021-10-28 13:15:07 +0200

    users -> users jsx

--- src/Components/UsersContainer/Users.jsx
+++ src/Components/UsersContainer/Users.jsx
@@ -2,6 +2,38 @@ import React from "react";
 import style from "./users.module.css";
 
 export default function Users(props) {
+  if (props.users.length === 0) {
+    props.setUsers([
+      {
+        id: 1,
+        photoUrl:
+          "https://t4.ftcdn.net/jpg/01/26/63/11/240_F_126631173_W9Nq8ZA5s0R0M3ZIBx3BMytVIFseGa9c.jpg",
+        fullName: "Vladimir",
+        status: "I'm russian",
+        followed: false,
+        location: { city: "Moscow", country: "Russia" },
+      },
+      {
+        id: 2,
+        photoUrl:
+          "https://t4.ftcdn.net/jpg/01/26/63/11/240_F_126631173_W9Nq8ZA5s0R0M3ZIBx3BMytVIFseGa9c.jpg",
+        fullName: "Franc",
+        status: "I'm french",
+        followed: true,
+        location: { city: "Paris", country: "France" },
+      },
+      {
+        id: 3,
+        photoUrl:
+          "https://t4.ftcdn.net/jpg/01/26/63/11/240_F_126631173_W9Nq8ZA5s0R0M3ZIBx3BMytVIFseGa9c.jpg",
+        fullName: "Jack",
+        status: "I'm American",
+        followed: true,
+        location: { city: "New York", country: "USA" },
+      },
+    ]);
+  }
+
   return (
     <div>
       {props.users.map((u) => (
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,35 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 let initialState = {
-  users: [
-    {
-      id: 1,
-      photoUrl:
-        "https://t4.ftcdn.net/jpg/01/26/63/11/240_F_126631173_W9Nq8ZA5s0R0M3ZIBx3BMytVIFseGa9c.jpg",
-      fullName: "Vladimir",
-      status: "I'm russian",
-      followed: false,
-      location: { city: "Moscow", country: "Russia" },
-    },
-    {
-      id: 2,
-      photoUrl:
-        "https://t4.ftcdn.net/jpg/01/26/63/11/240_F_126631173_W9Nq8ZA5s0R0M3ZIBx3BMytVIFseGa9c.jpg",
-      fullName: "Franc",
-      status: "I'm french",
-      followed: true,
-      location: { city: "Paris", country: "France" },
-    },
-    {
-      id: 3,
-      photoUrl:
-        "https://t4.ftcdn.net/jpg/01/26/63/11/240_F_126631173_W9Nq8ZA5s0R0M3ZIBx3BMytVIFseGa9c.jpg",
-      fullName: "Jack",
-      status: "I'm American",
-      followed: true,
-      location: { city: "New York", country: "USA" },
-    },
-  ],
+  users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
