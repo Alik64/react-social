97684f802d267c4e8c3e133df2694e72a8d6a4f3
Albert <albert.barsamov@gmail.com>
2021-11-14 23:53:15 +0100

    componentDid mount pour Axios

--- src/Components/UsersContainer/Users.jsx
+++ src/Components/UsersContainer/Users.jsx
@@ -6,6 +6,8 @@ import userPhoto from "../../assets/images/user.jpg";
 class Users extends React.Component {
   constructor(props) {
     super(props);
+  }
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
