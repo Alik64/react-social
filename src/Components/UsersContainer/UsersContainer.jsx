ab357214c4493211836e66b472bd37eb64d8456d
Albert <albert.barsamov@gmail.com>
2021-11-16 15:47:53 +0100

    refactoring MapDispatch to props

--- src/Components/UsersContainer/UsersContainer.jsx
+++ src/Components/UsersContainer/UsersContainer.jsx
@@ -90,4 +90,11 @@ let mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+export default connect(mapStateToProps, {
+  follow: followAC,
+  unfollow: unfollowAC,
+  setUsers: setUsersAC,
+  setCurrentPage: setCurrentPageAC,
+  setTotalUsersCount: setUsersTotalCountAC,
+  toggleIsFetching: toggleIsFetchingAC,
+})(UsersContainer);
