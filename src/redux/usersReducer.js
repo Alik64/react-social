1fb891fcea29cc402a1f83b63bfeb00987b9cd28
Albert <albert.barsamov@gmail.com>
2021-11-16 15:01:34 +0100

    maj

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,7 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
-
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 let initialState = {
   users: [],
   pageSize: 5,
@@ -45,6 +45,9 @@ const usersReducer = (state = initialState, action) => {
     case SET_TOTAL_USERS_COUNT: {
       return { ...state, totalUsersCount: action.count };
     }
+    case TOGGLE_IS_FETCHING: {
+      return { ...state, isFetching: action.isFetching };
+    }
 
     default:
       return state;
@@ -61,4 +64,9 @@ export const setUsersTotalCountAC = (totalUsersCount) => ({
   type: SET_TOTAL_USERS_COUNT,
   count: totalUsersCount,
 });
+export const toggleIsFetchingAC = (isFetching) => ({
+  type: TOGGLE_IS_FETCHING,
+  isFetching,
+});
+
 export default usersReducer;
