df826574c30c7340371a46100f20c8f51e8c7e81
Albert <albert.barsamov@gmail.com>
2021-11-18 14:18:00 +0100

    auth reducer

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,11 +1,28 @@
+import axios from "axios";
 import React from "react";
-
+import { connect } from "react-redux";
+import { setAuthUserData } from "./../../redux/authReducer";
 import Header from "./Header";
 
 class HeaderContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.data.resultCode === 0) {
+          let { userId, email, login } = response.data.data;
+          this.props.setAuthUserData(userId, email, login);
+        }
+      });
+  }
+
   render() {
     return <Header {...this.props} />;
   }
 }
 
-export default HeaderContainer;
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -11,6 +11,7 @@ let initialState = {
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER_DATA:
+
       return {
         ...state,
         ...action.data
@@ -25,7 +26,7 @@ const authReducer = (state = initialState, action) => {
       return state;
   }
 };
-export const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } });
+export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } });
 
 // export const toggleIsFetching = (isFetching) => ({
 //   type: TOGGLE_IS_FETCHING,
