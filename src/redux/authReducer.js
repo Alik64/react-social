c968942113f7bbd88cc9ba77906c849425f91046
Albert <albert.barsamov@gmail.com>
2021-11-18 12:14:19 +0100

    auth reducer terminé

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,5 +1,5 @@
-const FOLLOW = "FOLLOW";
-const UNFOLLOW = "UNFOLLOW";
+const SET_USER_DATA = "SET_USER_DATA";
+
 
 let initialState = {
   userId: null,
@@ -8,61 +8,28 @@ let initialState = {
   // isFetching: false,
 };
 
-const usersReducer = (state = initialState, action) => {
+const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FOLLOW:
+    case SET_USER_DATA:
       return {
         ...state,
-        users: state.users.map((u) => {
-          if (u.id === action.userId) {
-            return { ...u, followed: true };
-          }
-          return u;
-        }),
+        ...action.data
       };
 
-    case UNFOLLOW:
-      return {
-        ...state,
-        users: state.users.map((u) => {
-          if (u.id === action.userId) {
-            return { ...u, followed: false };
-          }
-          return u;
-        }),
-      };
 
-    case SET_USERS: {
-      return { ...state, users: action.users };
-    }
-    case SET_CURRENT_PAGE: {
-      return { ...state, currentPage: action.currentPage };
-    }
-    case SET_TOTAL_USERS_COUNT: {
-      return { ...state, totalUsersCount: action.count };
-    }
-    case TOGGLE_IS_FETCHING: {
-      return { ...state, isFetching: action.isFetching };
-    }
+    // case TOGGLE_IS_FETCHING: {
+    //   return { ...state, isFetching: action.isFetching };
+    // }
 
     default:
       return state;
   }
 };
-export const follow = (userId) => ({ type: FOLLOW, userId });
-export const unfollow = (userId) => ({ type: UNFOLLOW, userId });
-export const setUsers = (users) => ({ type: SET_USERS, users });
-export const setCurrentPage = (currentPage) => ({
-  type: SET_CURRENT_PAGE,
-  currentPage,
-});
-export const setUsersTotalCount = (totalUsersCount) => ({
-  type: SET_TOTAL_USERS_COUNT,
-  count: totalUsersCount,
-});
-export const toggleIsFetching = (isFetching) => ({
-  type: TOGGLE_IS_FETCHING,
-  isFetching,
-});
+export const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } });
 
-export default usersReducer;
+// export const toggleIsFetching = (isFetching) => ({
+//   type: TOGGLE_IS_FETCHING,
+//   isFetching,
+// });
+
+export default authReducer;
