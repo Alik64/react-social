bb1ff47108ff66ff02ec8c6d1942898012724d37
Albert <albert.barsamov@gmail.com>
2021-11-18 21:33:15 +0100

    with credentials

--- src/Components/UsersContainer/UsersContainer.jsx
+++ src/Components/UsersContainer/UsersContainer.jsx
@@ -18,7 +18,9 @@ class UsersContainer extends React.Component {
 
     axios
       .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+        withCredentials: true
+      })
       .then((response) => {
         this.props.toggleIsFetching(false);
         this.props.setUsers(response.data.items);
@@ -31,7 +33,9 @@ class UsersContainer extends React.Component {
 
     axios
       .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+        withCredentials: true
+      })
       .then((response) => {
         this.props.setUsers(response.data.items);
         this.props.toggleIsFetching(false);
