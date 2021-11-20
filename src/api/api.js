ef03f99488a8e2e68164840685c196b2f33dbf57
Albert <albert.barsamov@gmail.com>
2021-11-20 11:55:19 +0100

    followAPi

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,29 @@
 import axios from "axios";
 
-export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
-        withCredentials: true
-    }).then(response => response.data)
+const instance = axios.create({
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    withCredentials: true,
+    headers: {
+        "API-KEY": "6be562ec-a00d-4f86-84ca-4bf41f9245b1"
+    }
+
+})
+
+
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(
+            `users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data)
+    }
+
 }
 
+export const followAPI = {
+    follow(id) {
+        return instance.post(
+            `https://social-network.samuraijs.com/api/1.0/follow/${id}`)
+            .then(response => response.data)
+    }
+}
