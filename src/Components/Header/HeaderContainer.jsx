5d94db4c8e3df3ca7e4180816c54d9a3387fe110
Albert <albert.barsamov@gmail.com>
2021-11-22 19:58:02 +0100

    clean imports

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,7 +1,7 @@
-import axios from "axios";
+
 import React from "react";
 import { connect } from "react-redux";
-import { authAPI } from "../../api/api";
+
 import { setAuthUserData, isAuthMe } from "./../../redux/authReducer";
 import Header from "./Header";
 
