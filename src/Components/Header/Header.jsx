0c85855abc4b4312c1fb3b63976f06b0647ef93d
Albert <albert.barsamov@gmail.com>
2021-11-23 22:00:58 +0100

    reafactoring logo header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import style from "./Header.module.css";
+import reactLogo from './../../assets/images/logo-react.gif'
+import samuraiLogo from './../../assets/images/logo-samurai.png'
 
 export default function Header(props) {
   return (
     <header className={style.header}>
       <div className={style.logoDiv}  >
-        <img src="/logo-samurai.png" alt="samurai" className={style.logo} />
+        <img src={reactLogo} alt="react" className={style.logo} />
+        <img src={samuraiLogo} alt="react" className={style.logo_samurai} />
 
       </div>
       <h3>React Samourai  <span className={style.samourai}> 侍 </span> </h3>
