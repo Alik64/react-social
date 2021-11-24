3bf29115ca56ef4e057711090f583dfffe3b77b1
Albert <albert.barsamov@gmail.com>
2021-11-24 10:45:08 +0100

    fonctional component

--- src/Components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,30 +1,52 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class ProfileStatus extends Component {
+// export default class ProfileStatus extends Component {
 
-    state = {
-        editMode: false
+//     state = {
+//         editMode: false
+//     }
+//     activatedMode() {
+//         this.setState({
+//             editMode: true
+//         })
+//     }
+//     desactivatedMode() {
+//         this.setState({
+//             editMode: false
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+
+//                 {this.state.editMode
+//                     ? <div><input autoFocus={true} onBlur={() => this.desactivatedMode()} type="text" value={this.props.status} /></div>
+//                     : <div><span onClick={() => this.activatedMode()}>{this.props.status}</span></div>}
+
+//             </div>
+//         )
+//     }
+// }
+import React, { useState } from 'react'
+
+export default function ProfileStatus(props) {
+    const [editMode, setEditMode] = useState(false)
+
+    const activatedMode = () => {
+        setEditMode(true)
     }
-    activatedMode() {
-        this.setState({
-            editMode: true
-        })
-    }
-    desactivatedMode() {
-        this.setState({
-            editMode: false
-        })
+    const desactivatedMode = () => {
+        setEditMode(false)
     }
 
-    render() {
-        return (
-            <div>
 
-                {this.state.editMode
-                    ? <div><input autoFocus={true} onBlur={() => this.desactivatedMode()} type="text" value={this.props.status} /></div>
-                    : <div><span onClick={() => this.activatedMode()}>{this.props.status}</span></div>}
+    return (
+        <div>
+            {editMode
+                ? <div><input autoFocus={true} onBlur={() => desactivatedMode()} type="text" value={props.status} /></div>
+                : <div><span onClick={() => activatedMode()}>{props.status}</span></div>}
 
-            </div>
-        )
-    }
+        </div>
+    )
 }
