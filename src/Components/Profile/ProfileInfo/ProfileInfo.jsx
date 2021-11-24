b35621847fb785615a779fcb2cdac7cab918834f
Albert <albert.barsamov@gmail.com>
2021-11-24 13:06:05 +0100

    put photo

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,9 +3,26 @@ import Preloader from "../../commun/Preloader/Preloader";
 import style from "./ProfileInfo.module.css";
 import userPhoto from "./../../../assets/images/user.jpg"
 import ProfileStatus from "./ProfileStatus"
+import axios from "axios";
+
+
 export default function ProfileInfo(props) {
   if (!props.profile) {
     return <Preloader />;
+  }
+
+  const putPhoto = (e) => {
+    let photo = e.target.files[0]
+    let formData = new FormData()
+    formData.append('image', photo)
+
+    return axios.post('https://social-network.samuraijs.com/api/1.0/profile/photo', formData, {
+      withCredentials: true,
+      headers: {
+        "API-KEY": "6be562ec-a00d-4f86-84ca-4bf41f9245b1",
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(res => console.log(res.data))
   }
 
   return (
@@ -22,7 +39,10 @@ export default function ProfileInfo(props) {
         {" "}
 
         <img className={style.ava}
-          src={props.profile.photos.small != null ? props.profile.photos.small : userPhoto} alt="avatar" />{" "}
+          src={props.profile.photos.small != null ? props.profile.photos.small : userPhoto} alt="avatar" />
+
+        <input type="file" id="photo" onChange={putPhoto} />
+
         <h2>{props.profile.fullName}</h2>
         <h2>{props.profile.aboutMe}</h2>
         <ProfileStatus status="Hello!" />
