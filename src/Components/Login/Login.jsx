9a2523e9d0c08934eb897965f4c8baa58c660f5d
Albert <albert.barsamov@gmail.com>
2021-11-26 22:14:35 +0100

    Login FOrm created

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
+export default function LoginForm() {
+    return (
+        <form >
+            <div>
+                <input type={"text"} placeholder={"Login"} />
+
+            </div>
+            <div>
+                <input type={"password"} placeholder={'Password'} />
+            </div>
+            <div>
+                <input type={"checkbox"} /> Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+
+
+
 export default function Login() {
     return (
         <div>
-            <h1>Login page</h1>
+            <h1>Login</h1>
+            <LoginForm />
         </div>
     )
 }
