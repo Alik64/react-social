7f44612e5ae5b26f5e6d33b78d6b587551915cfc
Albert <albert.barsamov@gmail.com>
2021-11-27 23:49:17 +0100

    créationn formulaire

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -13,7 +13,16 @@ export default function Login(props) {
     return (
         <div>
             <h1>Login</h1>
+            <form action="">
+                <div>
+                    <input type="text" placeholder="name" />
+                </div>
+                <div>
+                    <input type="password" placeholder="password" />
+                </div>
+                <button>Sign in</button>
 
+            </form>
         </div>
 
     )
