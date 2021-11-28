810b9a4c194a041de2c287b4f014d678613c04b5
Albert <albert.barsamov@gmail.com>
2021-11-28 22:43:58 +0100

    error

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,30 +1,3 @@
 
 import React from 'react';
 
-
-
-
-
-
-
-
-
-export default function Login(props) {
-    return (
-        <div>
-            <h1>Login</h1>
-            <form action="">
-                <div>
-                    <input type="text" placeholder="name" />
-                </div>
-                <div>
-                    <input type="password" placeholder="password" />
-                </div>
-                <button>Sign in</button>
-
-            </form>
-        </div>
-
-    )
-}
-
