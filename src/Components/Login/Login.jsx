2d244d1bb74071e8045479e9b4fb1bc68da72bca
Albert <albert.barsamov@gmail.com>
2021-11-28 23:30:45 +0100

    Formik - export default

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,2 +1,29 @@
 import React from "react";
 import { Formik, Field, Form, FieldArray, ErrorMessage } from 'formik'
+
+const initialValues = {
+    friends: [
+        {
+            name: '',
+            email: ''
+        }
+    ]
+}
+
+const Login = () => (
+    <div>
+        <h1>Invite Friends</h1>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={values => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2))
+                }, 500)
+            }}
+        >
+            {() => <div>Foo</div>}
+        </Formik>
+    </div>
+)
+
+export default Login
