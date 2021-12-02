223c33f21a66a1a611cf230aa3eab9e48930fc06
Albert <albert.barsamov@gmail.com>
2021-12-02 23:22:04 +0100

    PAge login formik - OK

--- samuray-network/src/Components/Login/Login.jsx
+++ samuray-network/src/Components/Login/Login.jsx
@@ -1,30 +1,49 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+
 const initialValues = {
     login: "",
-    password: ""
+    password: "",
+    remember: true
 }
 const onSubmit = (values) => {
     console.log(values)
 }
 
 const validationSchema = Yup.object({
-    name: Yup.string().required('Required'),
-    email: Yup.string().email('Invalid email format').required('Required'),
+    login: Yup.string().required('Required'),
     password: Yup.string().required('Required')
-
 })
 
-const Login = () => (
-    <div>
-        <h1>Login</h1>
-        <Formik>
+
+
+export default function Login(props) {
+    return (
+        <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            validationSchema={validationSchema}>
+
+            <Form>
+                <h1>Login</h1>
+                <div>
+                    <Field type="text" id="login" name='login' placeholder="Login" autoComplete="your-login"></Field>
+                    <ErrorMessage name='login' />
+                </div>
+                <div>
+                    <Field type="password" id="password" name='password' placeholder="Your password" autoComplete="current-password"></Field>
+                    <ErrorMessage name='password' />
+
+                </div>
+                <div>
+                    <Field type="checkbox" id="remember" name="remember" /> <span>Remember me</span>
+                </div>
+                <button type="submit">Sign in</button>
+
+
+            </Form>
 
         </Formik>
-
-
-    </div>
-)
-
-export default Login
+    )
+}
