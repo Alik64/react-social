9ae84dc7f608d3e16a181cba680b35dcc6a38db9
Albert <albert.barsamov@gmail.com>
2021-12-02 23:44:36 +0100

    login page Css - ok

--- samuray-network/src/Components/Login/Login.jsx
+++ samuray-network/src/Components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import style from "./Login.module.css"
 
 const initialValues = {
     login: "",
@@ -20,30 +21,32 @@ const validationSchema = Yup.object({
 
 export default function Login(props) {
     return (
-        <Formik
-            initialValues={initialValues}
-            onSubmit={onSubmit}
-            validationSchema={validationSchema}>
+        <div className={style.formik}>
+            <Formik
 
-            <Form>
-                <h1>Login</h1>
-                <div>
-                    <Field type="text" id="login" name='login' placeholder="Login" autoComplete="your-login"></Field>
-                    <ErrorMessage name='login' />
-                </div>
-                <div>
-                    <Field type="password" id="password" name='password' placeholder="Your password" autoComplete="current-password"></Field>
-                    <ErrorMessage name='password' />
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}>
 
-                </div>
-                <div>
-                    <Field type="checkbox" id="remember" name="remember" /> <span>Remember me</span>
-                </div>
-                <button type="submit">Sign in</button>
+                <Form className={style.form}>
+                    <h1>Login</h1>
+                    <div className={style.field}>
+                        <Field type="text" id="login" name='login' placeholder="Login" autoComplete="your-login"></Field>
+                        <ErrorMessage name='login' className={style.error} />
+                    </div>
+                    <div className={style.field}>
+                        <Field type="password" id="password" name='password' placeholder="Your password" autoComplete="current-password"></Field>
+                        <ErrorMessage name='password' className={style.error} />
+                    </div>
+                    <div >
+                        <Field type="checkbox" id="remember" name="remember" /> <span>Remember me</span>
+                    </div>
+                    <button type="submit">Sign in</button>
 
 
-            </Form>
+                </Form>
 
-        </Formik>
+            </Formik>
+        </div>
     )
 }
