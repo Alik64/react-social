0d336f759d67e05ee71e79d67d9a43d4e09027d6
Albert <albert.barsamov@gmail.com>
2021-12-06 22:41:35 +0100

    text area css update

--- samuray-network/src/Components/FormControl/Elements/Textarea.js
+++ samuray-network/src/Components/FormControl/Elements/Textarea.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Field, ErrorMessage } from 'formik'
 import TextError from '../TextError'
+import styles from './Textarea.module.css'
 
 function Textarea(props) {
     const { name, label, ...rest } = props
     return (
-        <div className="form-control">
+        <div className={styles.form_control}>
             <label htmlFor={name}>{label}</label>
             <Field as='textarea' id={name} name={name} {...rest} />
             <ErrorMessage name={name} component={TextError} />
