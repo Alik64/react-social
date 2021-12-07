09fb110dd904fef5d0708efa395b5508c0109d5a
Albert <albert.barsamov@gmail.com>
2021-12-07 17:35:39 +0100

    if (!props.isAuth) return <Navigate to={"/login"} />

--- samuray-network/src/Components/Profile/Profile.jsx
+++ samuray-network/src/Components/Profile/Profile.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
+
 export default function Profile(props) {
+
   return (
     <div>
       <ProfileInfo profile={props.profile} status={props.status} updateUserStatus={props.updateUserStatus} />
