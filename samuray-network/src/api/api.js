54a4a3c64150f964d2b52a960e626f9a0cd599d4
Albert <albert.barsamov@gmail.com>
2021-12-07 15:39:52 +0100

    Logout

--- samuray-network/src/api/api.js
+++ samuray-network/src/api/api.js
@@ -62,9 +62,11 @@ export const authAPI = {
         return instance.get(`auth/me`)
     },
     login(email, password, rememberMe = false) {
-        return instance.post(`auth/login`, {
-            email, password, rememberMe
-        })
-    }
+        return instance.post(`auth/login`, { email, password, rememberMe })
+    },
+    logout() {
+        return instance.delete(`auth/login`)
+    },
+
 }
 
