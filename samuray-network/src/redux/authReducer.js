4b6094796cdc0b6c223bf537313cabf3a588f725
Albert <albert.barsamov@gmail.com>
2021-12-07 23:59:16 +0100

    setStatus(response.data.messages[0])

--- samuray-network/src/redux/authReducer.js
+++ samuray-network/src/redux/authReducer.js
@@ -41,13 +41,13 @@ export const getAuthUserData = () => (dispatch) => {
   });
 }
 
-export const login = (email, password, rememberMe) => (dispatch) => {
+export const login = (email, password, rememberMe, setStatus) => (dispatch) => {
   authAPI.login(email, password, rememberMe)
     .then((response) => {
       if (response.data.resultCode === 0) {
         dispatch(getAuthUserData())
       } else {
-        let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
+        setStatus(response.data.messages[0])
       }
     });
 }
