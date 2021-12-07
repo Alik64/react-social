190a1f9fd6f7d884e8797419fa7e422d722bca7d
Albert <albert.barsamov@gmail.com>
2021-12-07 14:52:44 +0100

    export const login = (email, password, rememberMe)

--- samuray-network/src/redux/authReducer.js
+++ samuray-network/src/redux/authReducer.js
@@ -45,4 +45,13 @@ export const getAuthUserData = () => (dispatch) => {
   });
 }
 
+export const login = (email, password, rememberMe) => (dispatch) => {
+  authAPI.me().then((response) => {
+    if (response.data.resultCode === 0) {
+      let { id, email, login } = response.data.data;
+      dispatch(setAuthUserData(id, email, login))
+    }
+  });
+}
+
 export default authReducer;
