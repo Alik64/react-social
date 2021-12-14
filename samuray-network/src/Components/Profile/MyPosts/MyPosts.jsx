d0f895678084bb474eafdfe8c5f4440324ca76a9
Albert <albert.barsamov@gmail.com>
2021-12-14 22:39:14 +0100

    MyPosts => ()

--- samuray-network/src/Components/Profile/MyPosts/MyPosts.jsx
+++ samuray-network/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,59 +1,60 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import AddPostForm from "./AddPostForm";
 
 import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-// export default function MyPosts(props) {
+const MyPosts = (props) => {
 
-//   let postElements = props.posts.map((post) => (
-//     <Post key={post.id} message={post.message} likesCount={post.likesCount} />
-//   ));
-
-
-
-//   const onAddPost = (values) => {
-//     props.addPost(values);
-//   };
-
-
-//   return (
-//     <div className={style.postBlock}>
-//       <h3>My posts</h3>
-//       <AddPostForm addPost={onAddPost} />
-//       <div className={style.posts}>{postElements}</div>
-//     </div>
-//   );
-// }
-
-
-export default class MyPosts extends PureComponent {
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   return nextProps !== this.props || nextState !== this.state
-  // }
-
-
-  render() {
-
-
-    let postElements = this.props.posts.map((post) => (
-      <Post key={post.id} message={post.message} likesCount={post.likesCount} />
-    ));
+  let postElements = props.posts.map((post) => (
+    <Post key={post.id} message={post.message} likesCount={post.likesCount} />
+  ));
 
 
 
-    const onAddPost = (values) => {
-      this.props.addPost(values);
-    };
+  const onAddPost = (values) => {
+    props.addPost(values);
+  };
 
 
-    return (
-      <div className={style.postBlock}>
-        <h3>My posts</h3>
-        <AddPostForm addPost={onAddPost} />
-        <div className={style.posts}>{postElements}</div>
-      </div>
-    );
-  }
+  return (
+    <div className={style.postBlock}>
+      <h3>My posts</h3>
+      <AddPostForm addPost={onAddPost} />
+      <div className={style.posts}>{postElements}</div>
+    </div>
+  );
 }
+
+export default MyPosts
+
+// export default class MyPosts extends PureComponent {
+
+//   // shouldComponentUpdate(nextProps, nextState) {
+//   //   return nextProps !== this.props || nextState !== this.state
+//   // }
+
+
+//   render() {
+
+
+//     let postElements = this.props.posts.map((post) => (
+//       <Post key={post.id} message={post.message} likesCount={post.likesCount} />
+//     ));
+
+
+
+//     const onAddPost = (values) => {
+//       this.props.addPost(values);
+//     };
+
+
+//     return (
+//       <div className={style.postBlock}>
+//         <h3>My posts</h3>
+//         <AddPostForm addPost={onAddPost} />
+//         <div className={style.posts}>{postElements}</div>
+//       </div>
+//     );
+//   }
+// }
