46b3aab0b46a6472f0fb5c0bb03118d0e41b757b
Albert <albert.barsamov@gmail.com>
2021-12-14 22:21:16 +0100

    shouldComponentUpdate(nextProps, nextState) {     return nextProps !== this.props   } in myPosts

--- samuray-network/src/Components/Profile/MyPosts/MyPosts.jsx
+++ samuray-network/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import AddPostForm from "./AddPostForm";
 
 import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-export default function MyPosts(props) {
+// export default function MyPosts(props) {
 
-  let postElements = props.posts.map((post) => (
-    <Post key={post.id} message={post.message} likesCount={post.likesCount} />
-  ));
-
-
-
-  const onAddPost = (values) => {
-    props.addPost(values);
-  };
+//   let postElements = props.posts.map((post) => (
+//     <Post key={post.id} message={post.message} likesCount={post.likesCount} />
+//   ));
 
 
-  return (
-    <div className={style.postBlock}>
-      <h3>My posts</h3>
-      <AddPostForm addPost={onAddPost} />
-      <div className={style.posts}>{postElements}</div>
-    </div>
-  );
+
+//   const onAddPost = (values) => {
+//     props.addPost(values);
+//   };
+
+
+//   return (
+//     <div className={style.postBlock}>
+//       <h3>My posts</h3>
+//       <AddPostForm addPost={onAddPost} />
+//       <div className={style.posts}>{postElements}</div>
+//     </div>
+//   );
+// }
+
+
+export default class MyPosts extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps !== this.props
+  }
+
+
+  render() {
+    console.log("RENDER YO")
+
+    let postElements = this.props.posts.map((post) => (
+      <Post key={post.id} message={post.message} likesCount={post.likesCount} />
+    ));
+
+
+
+    const onAddPost = (values) => {
+      this.props.addPost(values);
+    };
+
+
+    return (
+      <div className={style.postBlock}>
+        <h3>My posts</h3>
+        <AddPostForm addPost={onAddPost} />
+        <div className={style.posts}>{postElements}</div>
+      </div>
+    );
+  }
 }
